1c792709a47695ccd936a57e7229e86ae8bce714
ChelseyBommer <chelsey.bommer@hva.nl>
2023-04-14 12:17:23 +0200

    added: Player movement moving

--- Assets/Gameassets/Examplelevel/Scripts/PlayerMovement.cs
+++ Assets/Gameassets/Examplelevel/Scripts/PlayerMovement.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float horizontalMovement;
+    public float verticalMovement;
+    private Rigidbody rb;
+    // A field editable from inside Unity with a default value of 5
+    public float speed = 2f;
+
     void Start()
     {
-        
+
+        rb = GetComponent<Rigidbody>();
+        // This will stop the player game object from rotating
+        // Try to comment this line and see what happens
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        // This will detect forward and backward movement
+        horizontalMovement = Input.GetAxis("Horizontal");
+        // This will detect sideways movement
+        verticalMovement = Input.GetAxis("Vertical");
+
+        // Calculate the direction to move the player
+        Vector3 movementDirection = transform.forward * verticalMovement + transform.right * horizontalMovement;
+        // Move the player
+        rb.AddForce(movementDirection * speed, ForceMode.Force);
     }
 }
