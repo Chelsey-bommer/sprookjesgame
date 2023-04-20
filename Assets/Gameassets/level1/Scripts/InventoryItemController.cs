32a88c898b43f1589193fe6737e5143bcf700a4c
ChelseyBommer <chelsey.bommer@hva.nl>
2023-04-20 14:19:26 +0200

    update: inventory drop semi-working
    Position is still weird but it spawns behind player now!

--- Assets/Gameassets/level1/Scripts/InventoryItemController.cs
+++ Assets/Gameassets/level1/Scripts/InventoryItemController.cs
@@ -7,9 +7,13 @@ public class InventoryItemController : MonoBehaviour
 {
     Item item;
     public Button RemoveButton;
+    public GameObject myPrefab;
+
+    private Transform playerPos;
+    private float dropOffset = 1f;
     void Start()
     {
-        
+        playerPos = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -17,6 +21,7 @@ public class InventoryItemController : MonoBehaviour
     {
         InventoryManager.Instance.Remove(item);
         Destroy(gameObject);
+        Instantiate(myPrefab, playerPos.position + playerPos.forward * dropOffset, Quaternion.identity);
     }
 
     public void AddItem(Item newItem){
