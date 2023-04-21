2f31eea9c7e2a9ae27e05f62047bfdbb9ada01ec
ChelseyBommer <chelsey.bommer@hva.nl>
2023-04-21 12:49:10 +0200

    add: questscript

---
+++ Assets/Gameassets/level1/Scripts/Quest.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Quest : MonoBehaviour
+{
+
+    public Image questItem;
+    public Color completedColor;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
