ace7a710aedf831b003051bef049b037966ed8e2
ChelseyBommer <chelsey.bommer@hva.nl>
2023-05-13 22:42:27 +0200

    Revert "add: wobbly NPC's"
    This reverts commit cdd6297d8d4c4e3c5cee335b4a0f44a53a701d1c.

--- Assets/Gameassets/level1/Scripts/Quest.cs
+++ Assets/Gameassets/level1/Scripts/Quest.cs
@@ -51,7 +51,6 @@ public class Quest : MonoBehaviour
     private GameObject arrows;
 
     [SerializeField] public AudioSource questSoundeffect;
-   
 
     void Start()
     {
@@ -82,7 +81,7 @@ public class Quest : MonoBehaviour
         ColorUtility.TryParseHtmlString("#dddddd", out inactiveColor);
         ColorUtility.TryParseHtmlString("#eeeeee", out normalColor);
 
-        
+        quest1.color = activeColor;
         quest1.GetComponent<Button>().interactable = true;
         quest2.GetComponent<Button>().interactable = false;
         quest3.GetComponent<Button>().interactable = false;
